bbbc5630205d3dbd8b7da1e70e38f3a0a48ad4fd
David Govea <govea.d@gmail.com>
2011-08-03 15:20:12 -0700

    Port 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-    port = process.argv[2] || 81;
+    port = process.argv[2] || 80;
 
 http.createServer(function(request, response) {
 
