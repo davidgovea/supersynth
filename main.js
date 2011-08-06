4d2dc8ac3ea718f01220d76972e660cc69c31c67
David Govea <govea.d@gmail.com>
2011-08-06 00:03:08 -0700

    Initial sprite.. logic is lame still

--- main.js
+++ main.js
@@ -1,15 +1,149 @@
 (function(){
-	var	Game = new mibbu(700,500, "mibbu");
+	var HEIGHT	= 500,
+		WIDTH	= 700,
+		Game	= new mibbu(700,500, "mibbu");
 	Game.fps().init();
 	
-	var	mario	= new Game.spr('img/grey.png', 100, 100, 1, 0);
-		pipe	= new Game.spr('img/green.png', 100, 100, 1, 0),
+	 mario	= new Game.spr('standa.png', 350, 400, 18, 1);
+		var pipe	= new Game.spr('img/green.png', 100, 100, 1, 0),
 		boxes	= [
 			new Game.spr('img/yellow.png', 100, 100, 1, 0),
 			new Game.spr('img/yellow.png', 100, 100, 1, 0),
 			new Game.spr('img/yellow.png', 100, 100, 1, 0)
 		],
-		bg		= new Game.bg('img/bg.png', 3, "S", {x:0,y:0});
+		bg		= new Game.bg('img/bg.png', 3, "S", {x:0,y:0}),
+		
+		floor	= 0,
+		mheight	= 400,
+		mwidth	= 350,
+		mzero	= HEIGHT -floor - mheight,
+		mleft	= 0,
+		mright	= WIDTH-mwidth,
+		pipeh	= 140,
+		pipew	= 150,
+		pipex	= 485,
+		pipey	= HEIGHT - floor - pipeh,
+		pipetop	= mzero - pipeh,
+		
+		xacc	= 10,
+		max_vx	= 50,
+		jumpv	= 65,
+		gravity	= 1,
+		vscale	= 0.1;
+
+	_.extend(mario, {
+		vel: {
+			x: 0,
+			y: 0
+		},
+		pos: {
+			x: 30,
+			y: mzero
+		},
+		state:	0,
+		sides:	0,
+		width:	mwidth,
+		height:	mheight,
+		up:		0,
+		dir:	1,
+		mode:	function(mode, direction){
+			var	start	= false,
+				dir		= direction || 0,
+				num;
+				
+			switch(mode){
+			case 'stand':
+				if(mario.state !== 0){
+					start = true;
+					mario.state = 0;
+				}
+				num = 18;
+				break;
+				
+			case 'walk':
+				if(mario.state !== 1){
+					start = true;
+					mario.state = 1;
+				}
+				num = 15;
+				break;
+				
+			case 'jump':
+				if(mario.state !== 2){
+					start = true;
+					mario.state = 2;
+				}
+				num = 2;
+				break;
+				
+			default:
+			}
+			
+			if(start){
+				mario.change(mode+'a.png', 350, 400, num, 1);
+			}
+		//	if(dir !== mario.dir){
+				mario.animation(dir);
+				mario.dir = dir;
+		//	}
+			mario.position(mario.x, mario.y);
+			// console.log(mario.dir);
+			// console.log(dir);
+			return mario;
+		}
+	});
+	console.log(mario);
+	mario.size(mario.width,mario.height);
+	
+	mario.position(mario.pos.x, mario.pos.y, 6).speed(4);
+	
+	pipe.size(pipew, pipeh);
+	pipe.position(pipex, pipey, 2).speed(0);
+
+	boxes[0].position(50, 100).speed(0);
+	boxes[1].position(180, 100).speed(0);
+	boxes[2].position(310, 100).speed(0);
+	
+	bg.speed(0).dir(180).on();
+
+	function moveHandler(){
+		var state	= "stand", 
+			dir		;//= mario.dir;
+			
+		if(mario.sides){
+			state	= "walk";
+			dir		= (mario.sides<0) ? 0 : 1;
+			mario.vel.x	+= mario.sides * xacc;
+			mario.vel.x	= (Math.abs(mario.vel.x) > max_vx) ? mario.sides*max_vx : mario.vel.x;
+		} else {
+			if(mario.vel.x !== 0) {
+				mario.vel.x = Math.floor(mario.vel.x / 4);
+			}
+		}
+		
+
+		
+		if(mario.pos.y < mzero){
+			mario.vel.y += gravity;
+			state	= "jump"
+		} else {
+			mario.vel.y	= 0;
+			mario.pos.y	= mzero;
+		}
+		
+		if(mario.up){
+			if(mario.state !== 2){
+				mario.vel.y = -jumpv;
+				state = "jump";
+			}
+		}
+		
+		mario.pos.x	+= mario.vel.x * vscale;
+		mario.pos.y	+= mario.vel.y * vscale;
+		mario.mode(state, dir).position(mario.pos.x, mario.pos.y);
+		
+		
+	}
 	
 	
 	var catchKeyDown = function(e) {
@@ -18,22 +152,13 @@
 				//enter
 				break;
 			case 38: //up
-				mario.updown = -1;
-				break;
-			case 40: //down
-				mario.updown = 1;
+				mario.up = 1;
 				break;
 			case 37: //left
-				if(mario.touch !== -1){
-					mario.sides = -1;
-					mario.touch = 0;
-				}
+				mario.sides = -1;
 				break;
 			case 39: //right
-				if(mario.touch !==1){
-					mario.sides = 1;
-					mario.touch = 0;
-				}
+				mario.sides = 1;
 				break;
 		}
 		e.stopPropagation();
@@ -41,10 +166,7 @@
 	var catchKeyUp = function(e) {
 		switch (e.keyCode) {
 			case 38: //up
-				if (mario.updown===-1) mario.updown = 0;
-				break;
-			case 40: //down
-				if (mario.updown===1) mario.updown = 0;
+				if (mario.up===1) mario.up = 0;
 				break;
 			case 37: //left
 				if (mario.sides===-1) mario.sides = 0;
@@ -64,38 +186,20 @@
 		document.attachEvent("onkeyup",catchKeyUp);
 	}
 	
-	mario.size(90,140);
-	mario.position(30, 360, 6).speed(0);
-	mario.x = 30;
-	mario.y = 360;
 	
-	pipe.size(150,140);
-	pipe.position(485, 360, 2).speed(0);
 	
-	boxes[0].position(50, 100).speed(0);
-	boxes[1].position(180, 100).speed(0);
-	boxes[2].position(310, 100).speed(0);
 	
-	bg.speed(4).dir(~~(Math.random()*180)).on();
-	
-	var additionalLoop = function(){
-		if(mario.sides) mario.x += mario.sides * 4;
 		
-		if(mario.x < 0){ mario.sides = 0; mario.x = 0;}
-		
-		mario.position(mario.x,mario.y)
-		
-	};
+
 	mario.hit(pipe, function(){
-		mario.x += -mario.sides*2;
-		mario.position(mario.x, mario.y);
-		mario.touch = mario.sides;
-		mario.sides = 0;
+	//	mario.x = pipe.position().x-mario.size().width-1;
+		//mario.position(mario.x, mario.y);
+	//	mario.touch = mario.sides;
+		//mario.sides = 0;
 		
 	});
 	
-	Game.on();
-	Game.hook(additionalLoop).hitsOn();
+	Game.on().hook(moveHandler).hitsOn();
 	
 	
 		
